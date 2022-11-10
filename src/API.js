215d3278e856f0f438191d59661c3640ba0c2abd
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-11-10 22:53:48 +0200

    adds requests

--- src/API.js
+++ src/API.js
@@ -2,28 +2,38 @@ import axios from 'axios';
 export const getContactsApi = axios.create({
   baseURL: 'https://connections-api.herokuapp.com',
 });
-// Request User data - 4
-export const getNewUser = async body => {
-  const { data } = await getContactsApi.post('/users/signup', body);
+// Request User data
+export const getNewUser = async credentials => {
+  const { data } = await getContactsApi.post('/users/signup', credentials);
   return data;
 };
-export const loginUser = async body => {
-  const { data } = await getContactsApi.post('/users/login', body);
+export const loginUser = async credentials => {
+  const { data } = await getContactsApi.post('/users/login', credentials);
   return data;
 };
-
-// Request Contacts data - 4
+export const logOutUser = async credentials => {
+  const { data } = await getContactsApi.post('/users/logout', credentials);
+  return data;
+};
+export const currentUser = async credentials => {
+  const { data } = await getContactsApi.get('/users/current', credentials);
+  return data;
+};
+// Request Contacts data
 
 export const getContacts = async () => {
   const { data } = await getContactsApi.get('/contacts');
   return data;
 };
-
-export const addContacts = async body => {
-  const { data } = await getContactsApi.post('/contacts', body);
+export const addContacts = async credentials => {
+  const { data } = await getContactsApi.post('/contacts', credentials);
   return data;
 };
 export const removeContacts = async id => {
   const { data } = await getContactsApi.delete(`/contacts/${id}`);
+  return data;
+};
+export const UpdateContacts = async id => {
+  const { data } = await getContactsApi.patch(`/contacts/${id}`, id);
   return data;
 };
