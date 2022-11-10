3fbfe06ad8332e8657bebae1117c9952349dc10e
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-11-10 22:54:52 +0200

    adds persist

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 import { BrowserRouter } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter
+        // basename="/goit-react-hw-08-phonebook/"
+        >
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import { rootInitialState } from './root.initial-state';
 import { rootReducer } from './root.reducer';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
-  // reducer: persistedReducer,
   reducer: rootReducer,
   devTools: true,
   preloadState: rootInitialState,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
